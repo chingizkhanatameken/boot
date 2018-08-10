af7a730a6e74215e05a7f3f60b61081f4c67f26b
chingizkhantangirbergenov <chingizkhan.tangirbergenov@nu.edu.kz>
2018-08-10 13:31:11 +0600

    3

--- src/main/java/com/arocketman/github/BlogApplication.java
+++ src/main/java/com/arocketman/github/BlogApplication.java
@@ -2,9 +2,6 @@ package com.arocketman.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-
 @SpringBootApplication
 public class BlogApplication {
 
